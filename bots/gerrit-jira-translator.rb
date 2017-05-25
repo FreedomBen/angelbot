4fd1274e20fe84c9cb5fb341979f3595a56a08dd
Benjamin Porter <bporter@instructure.com>
2017-05-25 14:19:35 -0800

    Add TALENT prefix for jira tickets

--- bots/gerrit-jira-translator.rb
+++ bots/gerrit-jira-translator.rb
@@ -8,7 +8,7 @@ require_relative '../lib/gerrit-jira-translator/data'
 
 class GerritJiraTranslator < SlackbotFrd::Bot
   def whitelisted_prefixes
-    'CNVS|TD|MBL|OPS|SD|RD|ITSD|SE|DS|BR|CYOE|NTRS|PANDA|OUT|MC|PFS|FALCOR'
+    'CNVS|TD|MBL|OPS|SD|RD|ITSD|SE|DS|BR|CYOE|NTRS|PANDA|OUT|MC|PFS|FALCOR|TALENT'
   end
 
   def add_callbacks(slack_connection)
