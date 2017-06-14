62332eddf398a26d3375bcf9143092d3db2bf00e
Benjamin Porter <bporter@instructure.com>
2017-06-14 11:20:42 -0800

    add testrails

---
+++ bots/echo-chamber-bot.rb
@@ -0,0 +1,64 @@
+require 'slackbot_frd'
+
+class EchoChamberBot < SlackbotFrd::Bot
+  TRIGGER_WORDS = [
+    'linux',
+    'mac',
+    'android',
+    'ios',
+    'iphone',
+    'windows'
+  ]
+
+  GENERIC_STATEMENT_BASE = 'GENERIC_STATEMENT_BASE'
+  SUBJECT_STATEMENT_BASE = 'SUBJECT_STATEMENT_BASE'
+
+  def generic_statement(user)
+    [
+      "I hear you #{user}.",
+      "I couldn't agree more #{user}.",
+      "That is so true #{user}.",
+      "I'm glad you're here #{user}.",
+      "I feel the same way #{user}.",
+      "That's such a great point of view #{user}.",
+      "#{user} we are vibing here."
+    ].shuffle.first
+  end
+
+  def subject_statement(subject)
+    [
+      "oh yeah, #{SUBJECT_STATEMENT_BASE} is the greatest. :heart:",
+      "More people need #{SUBJECT_STATEMENT_BASE} in their lives.",
+      "Someday #{SUBJECT_STATEMENT_BASE} will take over the world."
+    ].shuffle.first.gsub(SUBJECT_STATEMENT_BASE, subject)
+  end
+
+  def desired_channel?(channel)
+    %w[echo_chamber bps_test_graveyard2].include?(channel)
+  end
+
+  def response(user, message)
+    tword = TRIGGER_WORDS.select{ |word| message.downcase.include?(word) }
+    if tword.count != 1
+      generic_statement(user)
+    else
+      subject_statement(tword.first)
+    end
+  end
+
+  def interested?(user, channel, message)
+    message && desired_channel?(channel) && user != :bot && user != 'angel'
+  end
+
+  def add_callbacks(slack_connection)
+    slack_connection.on_message do |user:, channel:, message:, timestamp:, thread_ts:|
+      if interested?(user, channel, message)
+        slack_connection.send_message(
+          channel: channel,
+          message: response(user, message),
+          thread_ts: thread_ts
+        )
+      end
+    end
+  end
+end
---
+++ lib/testrails/user.rb
@@ -0,0 +1,55 @@
+require 'httparty'
+require 'json'
+
+require_relative 'base'
+
+module Testrails
+  class Change
+    include HTTParty
+    base_uri "#{Testrails.base_url}"
+
+    def initialize(username:, password:)
+      @username = username
+      @password = password
+    end
+
+    def get_testcase(id)
+        ret =self.class.get("/get_case/#{id}", {
+            basic_auth: basic_auth,
+            headers: {
+            'Content-Type' => 'application/json'
+            },
+            timeout: 5
+        }).body.split("\n")
+        JSON.parse(ret.join("\n"))
+    end
+
+    def get_sections(section_id)
+        breadcrumbs = ""
+        current_id = section_id
+        loop do
+            ret =self.class.get("/get_section/#{current_id}", {
+                basic_auth: basic_auth,
+                headers: {
+                'Content-Type' => 'application/json'
+                },
+                timeout: 5
+            }).body.split("\n")
+            result = JSON.parse(ret.join("\n"))
+            breadcrumbs = " > " + result['name'] + breadcrumbs
+            break if result['depth'] == 0
+            current_id = result['parent_id']
+        end
+        breadcrumbs = breadcrumbs[3...breadcrumbs.length]
+    end
+
+    private
+
+    def basic_auth
+      {
+        username: @username,
+        password: @password
+      }
+    end
+  end
+end
