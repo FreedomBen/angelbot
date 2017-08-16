30e0cae26aed3ea6b8681dc49901f8bcb339a9e2
Benjamin Porter <bporter@instructure.com>
2017-08-16 11:34:26 -0600

    fix color name typo

--- scripts/docker-clean-images-unlabeled.sh
+++ scripts/docker-clean-images-unlabeled.sh
@@ -5,4 +5,4 @@ declare -rx color_cyan='\033[0;36m'
 
 echo -e "${color_cyan}Deleting all unlabled images...${color_restore}";
 docker rmi $(docker images | grep -iE '^<none>' | awk '{print $3}' | xargs)
-echo -e "${color_cyna}Done${color_restore}"
+echo -e "${color_cyan}Done${color_restore}"
