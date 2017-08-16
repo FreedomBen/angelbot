131a8975380c25100800ef632f2e3b0385dc55a7
Benjamin Porter <bporter@instructure.com>
2017-08-16 11:25:39 -0600

    Make scripts/start-bots.sh more smart and helpful

--- scripts/start-bots.sh
+++ scripts/start-bots.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/env bash
-. <(aescrypt -d angelbot.aes -o -) || echo 'Doh password was wrong'
-slackbot-frd start
+
+# if we're not in the docker container, run the container
+
+is_docker_user ()
+{
+  whoami | grep 'docker' >/dev/null 2>&1
+}
+
+if is_docker_user; then
+  . <(aescrypt -d angelbot.aes -o -) || echo 'Doh password was wrong'
+  slackbot-frd start
+else
+  echo "Looks like you aren't in the docker container..."
+  echo "I'll start it for you.  Once it starts, run this script inside it"
+  echo ""
+  echo "    ./scripts/start-bots.sh"
+  if [ -d "scripts" ]; then
+    ./scripts/run-container.sh
+  elif [ -x "run-container.sh" ]; then
+    ./run-container.sh
+  else
+    "Oh no, you must be in a weird working directory. Try from the project root"
+  fi
+fi
