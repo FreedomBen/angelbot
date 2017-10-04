51af829c5b95329a8daa02b2f871ce70f0f1a281
Dan Minkevitch <dan@instructure.com>
2017-10-04 09:05:09 -0700

    Add Admin & Comms JIRA Projects

--- bots/gerrit-jira-translator.rb
+++ bots/gerrit-jira-translator.rb
@@ -8,7 +8,7 @@ require_relative '../lib/gerrit-jira-translator/data'
 
 class GerritJiraTranslator < SlackbotFrd::Bot
   def whitelisted_prefixes
-    'CNVS|TD|MBL|OPS|SD|RD|ITSD|SE|DS|BR|CYOE|NTRS|PANDA|OUT|MC|PFS|FALCOR|TALENT|EISR|GOOF|GRADE|QUIZ|SEC|AMS|IMPENG'
+    'CNVS|TD|MBL|OPS|SD|RD|ITSD|SE|DS|BR|CYOE|NTRS|PANDA|OUT|MC|PFS|FALCOR|TALENT|EISR|GOOF|GRADE|QUIZ|SEC|AMS|IMPENG|ADMIN|COMMS'
   end
 
   def add_callbacks(slack_connection)
