285ccff4df5594eb11df8cc2af80ffc566a42493
Dan Minkevitch <dan@instructure.com>
2017-10-27 05:25:23 -0700

    Add CAT for JIRA translations

--- bots/gerrit-jira-translator.rb
+++ bots/gerrit-jira-translator.rb
@@ -8,7 +8,7 @@ require_relative '../lib/gerrit-jira-translator/data'
 
 class GerritJiraTranslator < SlackbotFrd::Bot
   def whitelisted_prefixes
-    'CNVS|TD|MBL|OPS|SD|RD|ITSD|SE|DS|BR|CYOE|NTRS|PANDA|OUT|MC|PFS|FALCOR|TALENT|EISR|GOOF|GRADE|QUIZ|SEC|AMS|IMPENG|ADMIN|COMMS'
+    'CNVS|TD|MBL|OPS|SD|RD|ITSD|SE|DS|BR|CYOE|NTRS|PANDA|OUT|MC|PFS|FALCOR|TALENT|EISR|GOOF|GRADE|QUIZ|SEC|AMS|IMPENG|ADMIN|COMMS|CAT'
   end
 
   def add_callbacks(slack_connection)
